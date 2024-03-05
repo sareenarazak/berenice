79ca327910510a9a8fc0cbffea006ae3ff678adf
sareenaabdulrazak <11263177+sareenarazak@users.noreply.github.com>
2024-03-05 16:58:46 -0500

    Added all 6 planets

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -35,10 +35,14 @@
 </script>
 <!--54:36:24:16:12:9-->
 <h1>Welcome to Berenice</h1>
-<svg width="300" height="200" xmlns="http://www.w3.org/2000/svg">
-  <circle cx="150" cy="100" r="80" fill="black"  on:click={handleClick}/>
-  <Planet name="bat" color="blue" radius="10" orbits="54" distance="50" time="{timeInput}"/>
-  <Planet name="cap" color="cyan" radius="13" orbits="36" distance="80" time="{timeInput}"/>
+<svg width="300" height="200" xmlns="http://www.w3.org/2000/svg" on:click={handleClick}>
+  <circle cx="150" cy="100" r="30" fill="black"/>
+  <Planet name="bat" color="blue" radius="10" orbits="54" distance="40" time="{timeInput}"/>
+  <Planet name="cap" color="cyan" radius="13" orbits="36" distance="50" time="{timeInput}"/>
+  <Planet name="drum" color="darkgreen" radius="15" orbits="24" distance="60" time="{timeInput}"/>
+  <Planet name="each" color="teal" radius="8" orbits="16" distance="70" time="{timeInput}"/>
+  <Planet name="fine" color="firebrick" radius="16" orbits="12" distance="90" time="{timeInput}"/>
+  <Planet name="gust" color="goldenrod" radius="20" orbits="9" distance="100" time="{timeInput}"/>
 </svg>
 <label for="time">Enter time</label>
 <input type="range" id="time" min="0" max="10000" bind:value="{timeInput}"/>

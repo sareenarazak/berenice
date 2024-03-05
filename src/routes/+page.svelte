f824126b91084a32270d468f888d36eda324c97c
sareenaabdulrazak <11263177+sareenarazak@users.noreply.github.com>
2024-03-05 16:50:36 -0500

    Extract planet to svelte component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,67 +1,45 @@
 <script>
-  const TAU = 2 * Math.PI;
+  /**
+   * Planet ==> name, color, radius, orbital period, distance
+   *  54:36:24:16:12:9
+   */
+  import Planet from './Planet.svelte';
+
   const CYCLE_TIME = 10_000;
+
   let timeInput = 0;
   let playing = false;
-  let bAngle = 0;
-  let rAngle = 0;
 
-  let bx = 150;
-  let by = 100;
-  let rx = 150;
-  let ry = 100;
   function handleClick() {
     playing = !playing;
     start();
   }
 
-  function updateTime() {
-    console.log(`timeInput is ${timeInput}`);
-    updatePosition(timeInput);
-  }
-
   function start() {
     if (playing) {
-      console.log("Playing");
       requestAnimationFrame(elapse);
-    } else {
-      console.log("Not playing");
     }
   }
 
   function elapse(currentTime, lastTime) {
     if(lastTime != null) {
-        timeInput += (currentTime - lastTime) ;
+        timeInput += (currentTime - lastTime) / 10;
         if(timeInput > CYCLE_TIME) {
           timeInput = timeInput % CYCLE_TIME;
         }
-        updatePosition(timeInput);
-
     }
     if(playing) {
       requestAnimationFrame(newTime => elapse(newTime, currentTime));
     }
   }
-
-  function updatePosition(time) {
-    bAngle = (time * TAU * 2) / CYCLE_TIME;
-    rAngle = (time * TAU * 3) / CYCLE_TIME;
-
-    bx = Math.cos(bAngle) * 80 + 150 ;
-    by = Math.sin(bAngle) * 80 + 100;
-    rx = Math.cos(rAngle) * 50 + 150 ;
-    ry = Math.sin(rAngle) * 50 + 100;
-  }
 </script>
-
+<!--54:36:24:16:12:9-->
 <h1>Welcome to Berenice</h1>
 <svg width="300" height="200" xmlns="http://www.w3.org/2000/svg">
-  <circle cx="150" cy="100" r="80" fill="blue"  on:click={handleClick}/>
-  <circle cx="{bx}" cy="{by}" r="10" fill="black"/>
-  <circle cx ="{rx}" cy="{ry}" r="13" fill="red"/>
-
+  <circle cx="150" cy="100" r="80" fill="black"  on:click={handleClick}/>
+  <Planet name="bat" color="blue" radius="10" orbits="54" distance="50" time="{timeInput}"/>
+  <Planet name="cap" color="cyan" radius="13" orbits="36" distance="80" time="{timeInput}"/>
 </svg>
 <label for="time">Enter time</label>
-<input type="range" id="time" min="0" max="10000" bind:value="{timeInput}" on:change={updateTime}/>
+<input type="range" id="time" min="0" max="10000" bind:value="{timeInput}"/>
 <label for="angle">Enter angle</label>
-<input type="text" id="angle" value="{bAngle}"/>
---
+++ src/routes/Planet.svelte
@@ -0,0 +1,25 @@
+<script>
+	export let name;
+	export let color;
+	export let radius;
+	export let orbits;
+	export let distance;
+	export let time;
+
+	const TAU = 2 * Math.PI;
+	const CYCLE_TIME = 10_000;
+
+ 	$:cx = calculateX(time);
+ 	$:cy = calculateY(time);
+
+ function calculateX(time) {
+	 return  Math.cos((time * TAU * orbits) / CYCLE_TIME) * distance + 150 ;
+ }
+
+ function calculateY(time) {
+	 return  Math.sin((time * TAU * orbits) / CYCLE_TIME) * distance + 100 ;
+
+ }
+</script>
+
+<circle cx="{cx}" cy="{cy}" r="{radius}" fill="{color}" />

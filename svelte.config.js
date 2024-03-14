bbd5d5f6b06b11ea4cffa7fe097ac2514c96f8ca
sareenarazak <11263177+sareenarazak@users.noreply.github.com>
2024-03-14 17:22:52 -0400

    Change base path to empty string

--- svelte.config.js
+++ svelte.config.js
@@ -12,7 +12,8 @@ const config = {
 
 		}),
 		paths: {
-			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+			base: ''
+				//process.argv.includes('dev') ? '' : process.env.BASE_PATH
 		}
 	}
 };
